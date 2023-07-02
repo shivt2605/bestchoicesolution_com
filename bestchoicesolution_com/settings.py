5df6c35f23d076f6c523d53e31c235489ef3c1b6
shivt2605 <126387832+shivt2605@users.noreply.github.com>
2023-07-02 23:31:06 +0530

    databace

--- bestchoicesolution_com/settings.py
+++ bestchoicesolution_com/settings.py
@@ -80,11 +80,23 @@ WSGI_APPLICATION = 'bestchoicesolution_com.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'bestchoicesolution',
+       'USER': 'bestchoicesolution',
+       'PASSWORD': 'TIWARI@123',
+       'HOST': 'localhost',
+       'PORT': '',
+   }
 }
 
 
