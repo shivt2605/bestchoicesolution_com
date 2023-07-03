5b0c94691444f0eff96cd413a9287751256597b5
shivt2605 <126387832+shivt2605@users.noreply.github.com>
2023-07-03 11:33:16 +0530

    fqa

--- bestchoicesolution_com/settings.py
+++ bestchoicesolution_com/settings.py
@@ -80,24 +80,24 @@ WSGI_APPLICATION = 'bestchoicesolution_com.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'bestchoicesolution',
-       'USER': 'bestchoicesolution',
-       'PASSWORD': 'TIWARI@123',
-       'HOST': 'localhost',
-       'PORT': '',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+#DATABASES = {
+ #  'default': {
+  #     'ENGINE': 'django.db.backends.postgresql',
+   #    'NAME': 'bestchoicesolution',
+    #   'USER': 'bestchoicesolution',
+     #  'PASSWORD': 'TIWARI@123',
+      # 'HOST': 'localhost',
+       #'PORT': '',
+   #}
+#}
 
 
 # Password validation
